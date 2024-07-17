343e3dc739a2f3043ba49e7ab27dc7e8f52dc03e
Unal Ege Gaznepoglu <ege.gaznepoglu@fau.de>
2024-07-17 11:44:02 +0200

    Apply the fix proposed in https://github.com/csteinmetz1/auraloss/pull/72/commits/3d5f934584faad54dcd657ef0e0bb3665efa66be

--- auraloss/freq.py
+++ auraloss/freq.py
@@ -219,9 +219,7 @@ class STFTLoss(torch.nn.Module):
             self.window,
             return_complex=True,
         )
-        x_mag = torch.sqrt(
-            torch.clamp((x_stft.real**2) + (x_stft.imag**2), min=self.eps)
-        )
+        x_mag = torch.clamp(torch.abs(x_stft), min=self.eps)
 
         # torch.angle is expensive, so it is only evaluated if the values are used in the loss
         if self.phs_used:
